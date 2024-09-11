811a896a65678ef73afc89c6f41cb9cd18094c8f
Rachel <rachelmarvin2002@gmail.com>
2024-09-11 16:52:40 +0800

    Update userAccount.js

--- js/userAccount.js
+++ js/userAccount.js
@@ -13,9 +13,11 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('mobile-dof-accounting').style.display = 'none';
         }
  
+        if (userData.userOrg === 'Admin') {
+            document.getElementById('sidebar-dof-accounting').style.display = 'block';
+            document.getElementById('mobile-dof-accounting').style.display = 'block';
+        }
 
-
-        
     } else {
         // Redirect to login page if user data is not found
         window.location.href = '/login';
